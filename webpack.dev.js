a3d0d005eef45150dd6669e03e9cbbe5a137c92f
Benson Toh <benson_7667@hotmail.com>
2019-07-13 11:07:08 +0800

    move webpack config file out from the webpack directory

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,25 +1,28 @@
 const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: "production",
-  entry: "../src/index.js",
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "../dist")
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist")
+  },
+  devServer: {
+    hot: true,
+    inline: true,
+    open: true
   },
   plugins: [
-    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "[Production] React + Webpack + ESlint Boilerplate",
+      title: "[DevServer] React + Webpack + ESlint Boilerplate",
       description: "Boilerplate setup for react, webpack and eslint",
-      template: "../src/index.html"
+      template: "./src/index.html"
     }),
     new webpack.DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify(
-        process.env.NODE_ENV || "production"
+        process.env.NODE_ENV || "development"
       ),
       "process.env.BUILD_ENV": JSON.stringify(
         process.env.BUILD_ENV || "development"
